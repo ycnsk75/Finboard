54f97b43a3af28107c6d3d791b805b26c5893e7c
Jay baik <ethiansjh@gmail.com>
2025-02-25 17:12:19 +0000

    update tf vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,30 +3,30 @@ provider "google" {
     region  = var.region
 }
 
-resource "google_service_account" "finboard_sa" {
-    account_id   = "finboard_sa"
+resource "google_service_account" "finboard-sa" {
+    account_id   = "finboard-sa"
     display_name = "Service Account for finboard project"
     description  = "Service account for managing finboard pipelines on GCP"
 }
 
-resource "google_service_account_key" "finboard_sa_key" {
-    service_account_id = google_service_account.finboard_sa.name
+resource "google_service_account_key" "finboard-sa-key" {
+    service_account_id = google_service_account.finboard-sa.name
 }
 
-resource "google_project_iam_member" "finboard_sa_iam" {
+resource "google_project_iam_member" "finboard-sa-iam" {
     for_each = toset(var.service_account_roles)
     project = var.project_id
     role    = each.value
-    member  = "serviceAccount:${google_service_account.finboard_sa.email}"
+    member  = "serviceAccount:${google_service_account.finboard-sa.email}"
 }
 
 output "service_account_email" {
-    value = google_service_account.finboard_sa.email
+    value = google_service_account.finboard-sa.email
     description = "Email of the created service account"
 }
 
 output "service_account_key" {
-    value = google_service_account_key.finboard_sa_key.private_key
+    value = google_service_account_key.finboard-sa-key.private_key
     description = "Private key of the created service account (base64 encoded)"
     sensitive = true
 }
