89d023a8d5825fe5a89eee9c10c906480e5ec963
mohamed.jerbi+dbt@gmail.com <mohamed.jerbi+dbt@gmail.com>
2025-03-01 13:32:52 +0000

    fix sources

--- models/staging/stock/stg_stock_carbon.sql
+++ models/staging/stock/stg_stock_carbon.sql
@@ -1,5 +1,5 @@
 with source as (
-    select * from {{ source('finboard', 'green_stock_carbon_bkp') }}
+    select * from {{ source('finboard', 'raw_green_stock_carbon') }}
 ),
 
 
@@ -7,9 +7,9 @@ renamed as (
     select
         Company as company_name,
         Ticker as ticker,
-        Electricity_Consumption_GW as electricity_consumption_gw,
-        CO2_Emissions as co2_emissions,
-        Electrical_Power_GW as electrical_power_gw,
+        Electricity_Consumption_GWh as electricity_consumption_gw,
+        CO2_Emissions_Mt as co2_emissions,
+        Electrical_Power_GWh as electrical_power_gw,
         current_timestamp() as _loaded_at
     from source
 )
